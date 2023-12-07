40c843a03e5186a83594313a935c68063cce5f0e
Gideon Daniel Giftson T <55153800+dangolfecho@users.noreply.github.com>
2023-12-08 01:22:07 +0530

    Update popup.js
    Changed the scrapeContentFromPage function to account for spans, and img tags. Allowed it to get links of the images. 
    Although this is primarily meant for Twitter, the way I wrote it has allowed it to be inadvertentely used for all sites. 
    Change is required inorder to make it Twitter specific.

--- extension/single-chrome/popup.js
+++ extension/single-chrome/popup.js
@@ -1,8 +1,10 @@
 let scrapeContent = document.getElementById('scrapeContent');
 let list = document.getElementById('contentList');
 
+prev = "first"
+count = 0
+
 chrome.runtime.onMessage.addListener((request,sender,sendResponse) => {
-       
      let paragraphs = request.paragraphs;
      if(paragraphs == null || paragraphs.length == 0) {
         let li = document.createElement("li");
@@ -13,25 +15,40 @@ chrome.runtime.onMessage.addListener((request,sender,sendResponse) => {
         paragraphs.forEach((paragraph) => {
             let li = document.createElement("li");
             li.innerText = paragraph;
+			if(paragraph.length > 5){
+				if(prev === paragraph){
+					console.log("Post " + count);
+					count += 1;
+				}
+				else{
+					console.log(paragraph);
+				}
+				prev = paragraph;
+			}
             list.appendChild(li);
         });
      }
 })
-scrapeContent.addEventListener("click", async() => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func: scrapeContentFromPage,
-    });    
-})
+scrapeContent.addEventListener("click", async() => {
+   	let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+ 	chrome.scripting.executeScript({
+   	    target: {tabId: tab.id},
+   	    func: scrapeContentFromPage,
+   	});
+});
 
 function scrapeContentFromPage() {
  
-    let para = document.querySelectorAll('p, div, section'); // Adjust for other elements containing paragraphs
+    let para = document.querySelectorAll('span, img'); // Adjust for other elements containing paragraphs
 
-    let paragraphTexts = Array.from(para).map(p => p.innerText);
-
-
+    let paragraphTexts = Array.from(para).map(p => {
+		if(p.tagName === 'SPAN'){
+			return p.innerText;
+		}
+		else{
+			return p.src;
+		}
+	});
     chrome.runtime.sendMessage({paragraphs: paragraphTexts});
 }
