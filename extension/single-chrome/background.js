b216f494e7cd982d7e50d914caa0091459913d15
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-12-09 00:57:49 +0530

    Update background.js - Removed the logic meant for a particular site from here to ensure the modularity of the code is maintained

--- extension/single-chrome/background.js
+++ extension/single-chrome/background.js
@@ -1,25 +1,10 @@
-prev = "first";
-count = 0;
-tc = 0;
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
      let paragraphs = request.paragraphs;
      if(paragraphs == null || paragraphs.length == 0) {
      }
      else {
         paragraphs.forEach((paragraph) => {
-			if(paragraph.length > 5){
-				if(prev === paragraph){
-					console.log("Post " + count);
-					count += 1;
-					tc++;
-				}
-				else{
-					console.log(paragraph);
-					tc++;
-				}
-				prev = paragraph;
-			}
+			console.log(paragraph);
         });
      }
 })
