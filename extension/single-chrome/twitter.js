5f8c5c4dbd8e6fb12364111a391c68c6ce2c8561
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-12-09 00:56:59 +0530

    Update twitter.js - Included the scraping logic here instead of in the service worker

--- extension/single-chrome/twitter.js
+++ extension/single-chrome/twitter.js
@@ -1,3 +1,6 @@
+prev = "first";
+count = 0;
+
 function scrapeContentFromPage() {
  
     let para = document.querySelectorAll('span, img'); // Adjust for other elements containing paragraphs
@@ -10,6 +13,33 @@ function scrapeContentFromPage() {
 			return p.src;
 		}
 	});
-    chrome.runtime.sendMessage({paragraphs: paragraphTexts});
+	let flag = false;
+	let finalArray = [];
+	paragraphTexts.forEach(element => {
+		if(flag){
+			if(element === 'Terms of Service'){
+				flag = false;
+			}
+			else if( typeof element === 'string' ){
+				if (element.length > 5){
+					if(prev === element){
+						finalArray.push("Post " + count);
+						count += 1;
+					}
+					else{
+						finalArray.push(element);
+					}
+				}
+			}
+		}
+		else{
+			if(element == 'Following'){
+				flag = true;
+			}
+		}
+		prev = element;
+	});
+
+    chrome.runtime.sendMessage({paragraphs: finalArray});
 }
 setTimeout(scrapeContentFromPage, 5000);
