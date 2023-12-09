2008baf527da7d52acec9841ce75c7d2f6350a5e
aswin627946 <aswin885274@gmail.com>
2023-12-10 00:19:24 +0530

    facebook,twiiter scraping
    Added scraping function to be called everytime  we scroll the page to get new posts

--- extension/single-chrome/background.js
+++ extension/single-chrome/background.js
@@ -1,10 +1,10 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-     let paragraphs = request.paragraphs;
-     if(paragraphs == null || paragraphs.length == 0) {
-     }
-     else {
-        paragraphs.forEach((paragraph) => {
-			console.log(paragraph);
-        });
-     }
-})
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//      let paragraphs = request.paragraphs;
+//      if(paragraphs == null || paragraphs.length == 0) {
+//      }
+//      else {
+//         paragraphs.forEach((paragraph) => {
+// 			console.log(paragraph);
+//         });
+//      }
+// })
