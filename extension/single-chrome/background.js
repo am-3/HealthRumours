a75ad2dba8b184a4e7c2673dd3f50271434a0c4e
am-3 <53950633+am-3@users.noreply.github.com>
2023-12-18 12:50:02 +0530

    Resolved merge conflicts

--- extension/single-chrome/background.js
+++ extension/single-chrome/background.js
@@ -30,3 +30,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         });
      }
 })
+
+function logger (){
+        console.clear();
+        let selectedText = '';
+        const selection = window.getSelection();
+
+        if (selection && selection.toString()) {
+          selectedText = selection.toString();
+          console.log("Selected text:", selectedText);
+        } else {
+          console.log("No text selected.");
+        }
+}
+
+chrome.action.onClicked.addListener(function (tab) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: logger,
+      })
+});
