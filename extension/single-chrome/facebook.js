166c5d766b2c30272057e5eecf574b69193911e2
Gideon Daniel Giftson T <55153800+dangolfecho@users.noreply.github.com>
2023-12-18 12:57:50 +0530

    Update facebook.js - Removed artifacts from merging

--- extension/single-chrome/facebook.js
+++ extension/single-chrome/facebook.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-prev = "first";
-count = 0;
-
-function scrapeContentFromPage() {
- 
-    let para = document.querySelectorAll('div, img'); // Adjust for other elements containing paragraphs
-
-    let paragraphTexts = Array.from(para).map(p => {
-		if(p.tagName === 'DIV'){
-			return p.innerText;
-		}
-		else{
-			return p.src;
-		}
-	});
-	let flag = true;
-	let finalArray = [];
-	paragraphTexts.forEach(element => {
-		if(flag){
-			if(element === 'Terms of Service'){
-				flag = false;
-			}
-			else if( typeof element === 'string' ){
-				if (element.length > 5){
-					if(prev === element){
-						finalArray.push("Post " + count);
-						count += 1;
-					}
-					else{
-						finalArray.push(element);
-					}
-				}
-			}
-		}
-		else{
-			if(element == 'Following'){
-				flag = true;
-			}
-		}
-		prev = element;
-	});
-
-    chrome.runtime.sendMessage({paragraphs: finalArray});
-}
-
-setTimeout(scrapeContentFromPage, 5000);
-=======
 function scrapeContentFromPage() {
     console.clear();
  
@@ -92,4 +44,3 @@ function scrapeContentFromPage() {
 }
 setTimeout(scrapeContentFromPage, 5000);
 window.addEventListener('scroll', scrapeContentFromPage);
->>>>>>> 0f7b6e900c805d8de35eb3d0e069af570be41d9f
