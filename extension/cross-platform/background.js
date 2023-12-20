832ce08cd85da3c772079c9bf9dd0c61009b533d
MahithaKancharla <98204725+MahithaKancharla@users.noreply.github.com>
2023-12-20 09:53:33 +0530

    firefox telegram commit

--- extension/cross-platform/background.js
+++ extension/cross-platform/background.js
@@ -1,9 +1,24 @@
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: function () {
-      const allText = document.body.innerText;
-      console.log("Text extracted from the page:", allText);
+browser.runtime.onInstalled.addListener(() => {
+  function createContextMenuItem() {
+    browser.contextMenus.create({
+      id: "yourContextMenuId",
+      title: "Your Context Menu Option",
+      contexts: ["all"]
+    });
+  }
+  
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message && message.action === "createContextMenu") {
+      createContextMenuItem();
     }
-  });
+  }); 
+});
+
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+  console.log("user clicked");
+  if (info.menuItemId === "yourContextMenuId") {
+    console.log("before sending message retrieveCursorPosition");
+    browser.tabs.sendMessage(tab.id, { action: "executeCustomAction" });
+    console.log("after sending message retrieveCursorPosition");
+  }
 });
