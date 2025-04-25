760b9542f2caad60027269b484669c67d1b70283
Atharva More <211105@iiitt.ac.in>
2025-04-25 08:03:10 +0530

    updated git ignore

--- Backend/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-admin.site.register(WhatsappAndTelegram)
-admin.site.register(UserSelected)
-admin.site.register(NewsArticles)
-admin.site.register(SocialMediaPosts)
-admin.site.register(FacebookPosts)
